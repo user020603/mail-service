c0ffdd95b7675e0a942766e4caeed3a27f951196
user020603 <ptit.tienthanh@gmail.com>
2025-06-09 16:46:29 +0700

    update(mail/delivery): update report request

--- internal/delivery/rest/mail_handler.go
+++ internal/delivery/rest/mail_handler.go
@@ -1,49 +1,61 @@
 package rest
 
 import (
-	"strconv"
+	"fmt"
+	"net/http"
 	"thanhnt208/mail-service/config"
+	"thanhnt208/mail-service/internal/dto"
 	"thanhnt208/mail-service/internal/service"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 type MailHandler struct {
 	mailService service.IMailService
+	cfg         *config.Config
 }
 
-func NewMailHandler(mailService service.IMailService) *MailHandler {
+func NewMailHandler(mailService service.IMailService, cfg *config.Config) *MailHandler {
 	return &MailHandler{
 		mailService: mailService,
+		cfg:         cfg,
 	}
 }
 
-func (h *MailHandler) SendManualContainerReport(c *gin.Context, cfg config.Config) error {
-	startTime := c.Query("start_time")
-	endTime := c.Query("end_time")
-	if startTime == "" || endTime == "" {
-		c.JSON(400, gin.H{"error": "start_time and end_time are required"})
+func (h *MailHandler) SendManualContainerReport(c *gin.Context) error {
+	var req *dto.ReportRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid request data",
+		})
+	}
+
+	layout := "2006-01-02"
+	startTime, startErr := time.Parse(layout, req.StartDate)
+	endTime, endErr := time.Parse(layout, req.EndDate)
+	if startErr != nil || endErr != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid date format. Use YYYY-MM-DD.",
+		})
 		return nil
 	}
 
-	startTimeInt, err := strconv.ParseInt(startTime, 10, 64)
-	if err != nil {
-		c.JSON(400, gin.H{"error": "invalid start_time format"})
+	endTime = endTime.Add(23 * time.Hour).Add(59 * time.Minute).Add(59 * time.Second)
+	adminAddrConf := *h.cfg
+	adminAddrConf.AdminEmailAddr = req.AdminEmail
+
+	if err := h.mailService.SendManualContainerReport(&adminAddrConf, startTime.Unix(), endTime.Unix()); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to send report: " + err.Error(),
+		})
 		return nil
 	}
 
-	endTimeInt, err := strconv.ParseInt(endTime, 10, 64)
-	if err != nil {
-		c.JSON(400, gin.H{"error": "invalid end_time format"})
-		return nil
-	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("Report for period %s to %s has been sent to %s.", req.StartDate, req.EndDate, req.AdminEmail),
+	})
 
-	err = h.mailService.SendManualContainerReport(&cfg, startTimeInt, endTimeInt)
-	if err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
-		return nil
-	}
-
-	c.JSON(200, gin.H{"message": "Report sent successfully"})
 	return nil
 }
