29b0eb9e35500e82258afdcf8657d32695d2ae6e
user020603 <ptit.tienthanh@gmail.com>
2025-06-09 16:47:21 +0700

    update(mail/routes): update send mail route

--- api/routes/mail_routes.go
+++ api/routes/mail_routes.go
@@ -1,18 +1,16 @@
 package routes
 
 import (
-	"thanhnt208/mail-service/config"
 	"thanhnt208/mail-service/internal/delivery/rest"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupMailRoutes(h *rest.MailHandler) *gin.Engine {
-	cfg := config.LoadConfig()
 	router := gin.Default()
 
-	router.POST("mail/manual_container_report", func(c *gin.Context) {
-		_ = h.SendManualContainerReport(c, *cfg)
+	router.POST("mail/send_period_report", func(c *gin.Context) {
+		_ = h.SendManualContainerReport(c)
 	})
 
 	return router
