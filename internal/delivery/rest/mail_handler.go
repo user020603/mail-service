dbf438f1e7513f92bb5cc979919ee80dc31fb250
user020603 <ptit.tienthanh@gmail.com>
2025-07-02 11:04:34 +0700

    update(mail/delivery): add uptime duration handler

--- internal/delivery/rest/mail_handler.go
+++ internal/delivery/rest/mail_handler.go
@@ -59,3 +59,41 @@ func (h *MailHandler) SendManualContainerReport(c *gin.Context) error {
 
 	return nil
 }
+
+func (h *MailHandler) SendUptimeReport(c *gin.Context) error {
+	var req *dto.ReportRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid request data",
+		})
+	}
+
+	layout := "2006-01-02"
+    startTime, startErr := time.ParseInLocation(layout, req.StartDate, time.Local)
+    endTime, endErr := time.ParseInLocation(layout, req.EndDate, time.Local)
+	if startErr != nil || endErr != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid date format. Use YYYY-MM-DD.",
+		})
+		return nil
+	}
+
+	endTime = endTime.Add(23 * time.Hour).Add(59 * time.Minute).Add(59 * time.Second)
+	adminAddrConf := *h.cfg
+	adminAddrConf.AdminEmailAddr = req.AdminEmail
+
+	if err := h.mailService.SendUptimeReport(&adminAddrConf, startTime.Unix(), endTime.Unix()); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to send report: " + err.Error(),
+		})
+		return nil
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("Uptime Report for period %s to %s has been sent to %s.", req.StartDate, req.EndDate, req.AdminEmail),
+	})
+
+	return nil
+}
+
