2606f355fe22cec3bd7ec62d4f8de8a4af90693f
user020603 <ptit.tienthanh@gmail.com>
2025-07-11 12:07:41 +0700

    update(mail/routes): refactor mail routes

--- api/routes/mail_routes.go
+++ api/routes/mail_routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"thanhnt208/mail-service/api/middlewares"
 	"thanhnt208/mail-service/internal/delivery/rest"
 
 	"github.com/gin-gonic/gin"
@@ -9,13 +10,19 @@ import (
 func SetupMailRoutes(h *rest.MailHandler) *gin.Engine {
 	router := gin.Default()
 
-	router.POST("/send_period_report", func(c *gin.Context) {
-		_ = h.SendManualContainerReport(c)
-	})
+	router.POST("/send_uptime_ratio",
+		middlewares.JWTAuthMiddleware(),
+		middlewares.CheckScopeMiddleware("mail:send_uptime_ratio"),
+		func(c *gin.Context) {
+			_ = h.SendManualContainerReport(c)
+		})
 
-	router.POST("/send_uptime_report", func(c *gin.Context) {
-		_ = h.SendUptimeReport(c)
-	})
+	router.POST("/send_uptime_duration",
+		middlewares.JWTAuthMiddleware(),
+		middlewares.CheckScopeMiddleware("mail:send_uptime_duration"),
+		func(c *gin.Context) {
+			_ = h.SendUptimeReport(c)
+		})
 
 	return router
 }
